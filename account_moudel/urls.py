e8dde08015cbe00ed9598970cf7e54f9f3517839
Mahdi <mahdi.soltanian05@gmail.com>
2024-02-18 16:09:31 +0330

    Change the login_view function base view to loginview class base view in the url

--- account_moudel/urls.py
+++ account_moudel/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register_page'),
-    path('login/', views.login_view, name='login_page')
+    path('login/', views.loginview.as_view(), name='login_page')
 ]
