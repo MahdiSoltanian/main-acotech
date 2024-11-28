ac617b785d882a54b391c5fed82e83dae113c67f
Mahdi <mahdi.soltanian05@gmail.com>
2024-11-28 14:11:19 +0330

    make the phone_active_code to CharField

--- account_moudel/models.py
+++ account_moudel/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=18)
     password = models.CharField(max_length=100)
-    phone_active_code = models.IntegerField(null=True)
+    phone_active_code = models.CharField(null=True,max_length=18)
     class Meta:
         verbose_name = 'user'
         verbose_name_plural = 'users'
--- account_moudel/views.py
+++ account_moudel/views.py
@@ -77,7 +77,7 @@ def verify_number(request):
             linenumber= 30007487129352,
         )
         sms_ir.send_verify_code(
-        number="+989130923984",
+        number="phone_number",
         template_id=10000,
         parameters=[
             {
