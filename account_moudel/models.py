d32db4a9df4d6be3ddf7d61d4383313d309b09b9
Mahdi <mahdi.soltanian05@gmail.com>
2024-12-09 20:26:31 +0330

    working on displaying datas7

--- account_moudel/models.py
+++ account_moudel/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=18)
+    phone_number2 = models.CharField(max_length=18)
     password = models.CharField(max_length=100)
     class Meta:
         verbose_name = 'user'
