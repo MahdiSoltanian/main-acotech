9baff1a31c220d5167b8790dc97197c3862305f3
Mahdi <mahdi.soltanian05@gmail.com>
2024-12-09 19:25:49 +0330

    working on displaying datas

--- show_data/views.py
+++ show_data/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from api_moudel.models import Data
 
 def display_data(request):
-    data = Data.objects.all().order_by('-timestamp')  # Get all records, ordered by timestamp
+    data = Data.objects.all().order_by('-temp')  # Get all records, ordered by timestamp
     return render(request, 'flot.html', {'data': data})
