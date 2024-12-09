76db39e9ca32eeb3fdfeeb89331d9d8d2b5cab77
Mahdi <mahdi.soltanian05@gmail.com>
2024-12-09 19:49:20 +0330

    working on displaying datas3

--- show_data/views.py
+++ show_data/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 from api_moudel.models import Data
 
 def display_data(request):
-    data = Data.objects.all().order_by('-temp')  # Get all records, ordered by timestamp
-    return render(request, 'flot.html', {'data': data})
+    data = Data.objects.order_by('-id')[:10]  # Get the latest 10 records
+    response = {
+        "api_Key_Name": [temp.api_Key_Name for temp in data],
+        "sensor_Name": [temp.value for temp in data],
+        "temp": [temp.value for temp in data],
+    }
+    return JsonResponse(response)
+    return render(request, 'flot.html')
+
